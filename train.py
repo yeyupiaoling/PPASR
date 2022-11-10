df51dc5aa2fabe70e13691238247540990708c81
夜雨飘零 <yeyupiaoling@foxmail.com>
2022-11-10 17:06:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ from ppasr.utils.utils import add_arguments, print_arguments
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('configs',          str,    'configs/conformer_online_zh.yml',       '配置文件')
-add_arg("local_rank",       int,    0,                             '多卡训练的本地GPU')
 add_arg("use_gpu",          bool,   True,                          '是否使用GPU训练')
 add_arg('augment_conf_path',str,    'configs/augmentation.json',   '数据增强的配置文件，为json格式')
 add_arg('save_model_path',  str,    'models/',                  '模型保存的路径')
